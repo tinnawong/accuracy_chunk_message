1732c5a9de5cccfabffeae4796556f9714fd9ded
tinnawong <tinnawong2010@hotmail.com>
2020-09-16 17:08:52 +0700

    add case WER method

--- prove.py
+++ prove.py
@@ -44,7 +44,7 @@ def generateMatrix(r, h):
     return dimensions
 
 
-def findLastIndex(r, h, dimensions,threshold ):
+def findLastIndex(r, h, dimensions, threshold):
     # print("dimensions id (findLastIndex) ", id(dimensions))
     x = len(r)
     y = len(h)
@@ -55,15 +55,23 @@ def findLastIndex(r, h, dimensions,threshold ):
         if x == 0 or y == 0:
             break
         # print(r[x - 1], " vs ", h[y - 1], " = ", x, y)
-        if r[x - 1] == h[y - 1]:
+        if(x == 0 and y > 0):
+            # insertion
+            y = y - 1
+            countCharlecter = 0
+        elif(x > 0 and y == 0):
+            # deletion
+            x = x - 1
+            countCharlecter = 0
+        elif r[x - 1] == h[y - 1]:
             x = x - 1
             y = y - 1
             countCharlecter += 1
-            if(countCharlecter ==1):
-                lastIndex=[x,y]
-            if(countCharlecter>=threshold):
+            if(countCharlecter == 1):
+                lastIndex = [x, y]
+            if(countCharlecter >= threshold):
                 return lastIndex
-            
+
             # html = '%s ' % h[y] + html
         elif dimensions[x][y] == dimensions[x - 1][y - 1] + 1:    # substitution
             x = x - 1
@@ -85,10 +93,10 @@ def findLastIndex(r, h, dimensions,threshold ):
     return lastIndex
 
 
-def getChunk(r, h,threshold ):
-    print("chunk : ",r," >> ",h)
+def getChunk(r, h, threshold):
+    print("chunk : ", r, " >> ", h)
     dimensions = generateMatrix(r, h)
-    lastIndex = findLastIndex(r, h, dimensions,threshold )
+    lastIndex = findLastIndex(r, h, dimensions, threshold)
     # try:
     #     text = "ผลของเฉลย : "+str(r[:lastIndex[0]])+"\nผลของตัวเทียบ : "+h[:lastIndex[1]]
     # except Exception as e:
@@ -100,119 +108,176 @@ def getChunk(r, h,threshold ):
     #     file.write(str(text))
     return lastIndex
 
+
 def WER(r, h):
-    print("WER :",r," <<>> ",h)
+    print("WER :", r, " <<>> ", h)
     """
     Given two list of strings how many word error rate(insert, delete or substitution).
     """
-    d = numpy.zeros((len(r) + 1) * (len(h) + 1), dtype=numpy.uint16)
-    d = d.reshape((len(r) + 1, len(h) + 1))
+    dimensions = numpy.zeros((len(r) + 1) * (len(h) + 1), dtype=numpy.uint16)
+    dimensions = dimensions.reshape((len(r) + 1, len(h) + 1))
     for i in range(len(r) + 1):
         for j in range(len(h) + 1):
             if i == 0:
-                d[0][j] = j
+                dimensions[0][j] = j
             elif j == 0:
-                d[i][0] = i
+                dimensions[i][0] = i
 
     for i in range(1, len(r) + 1):
         for j in range(1, len(h) + 1):
             if r[i - 1] == h[j - 1]:
-                d[i][j] = d[i - 1][j - 1]
+                dimensions[i][j] = dimensions[i - 1][j - 1]
             else:
-                substitution = d[i - 1][j - 1] + 1
-                insertion = d[i][j - 1] + 1
-                deletion = d[i - 1][j] + 1
-                d[i][j] = min(substitution, insertion, deletion)
+                substitution = dimensions[i - 1][j - 1] + 1
+                insertion = dimensions[i][j - 1] + 1
+                deletion = dimensions[i - 1][j] + 1
+                dimensions[i][j] = min(substitution, insertion, deletion)
+    # result = float(dimensions[len(r)][len(h)]) / len(r) * 100
 
     x = len(r)
     y = len(h)
     substitution = 0
     deletion = 0
     insertion = 0
-
+    correct = 0
     while True:
         if x == 0 or y == 0:
             break
-        if r[x - 1] == h[y - 1]:
+        if(x == 0 and y > 0):
+            # insertion
+            y = y - 1
+            insertion = insertion +1
+        elif(x > 0 and y == 0):
+            # deletion
+            x = x - 1
+            deletion = deletion + 1
+
+        elif(r[x-1] == h[y-1]):
+            # correct
+            x = x-1
+            y = y-1
+            correct = correct + 1
+        elif dimensions[x][y] == dimensions[x - 1][y - 1] + 1:    # substitution
             x = x - 1
             y = y - 1
-        elif d[x][y] == d[x - 1][y - 1] + 1:    # substitution
+            substitution = substitution + 1
+        elif dimensions[x][y] == dimensions[x - 1][y] + 1:        # deletion
             x = x - 1
+            deletion = deletion + 1
+        elif dimensions[x][y] == dimensions[x][y - 1] + 1:        # insertion
             y = y - 1
-            substitution+=1
-        elif d[x][y] == d[x - 1][y] + 1:        # deletion
-            x = x - 1
-            deletion += 1
-        elif d[x][y] == d[x][y - 1] + 1:        # insertion
-            y = y - 1
-            insertion += 1
+            insertion = insertion +1
         else:
             print('\nWe got an error.')
             break
-    print(">>>",substitution,deletion,insertion)
-    # result = float((substitution+deletion+insertion)/len(r))
-    return (substitution,deletion,insertion)
+    print(">>>", substitution, deletion, insertion, correct)
+    return (substitution, deletion, insertion, correct)
 
-def measureByWER(r, h,threshold,chunkSize):
-    from tt import get_word_error_rate
+
+def measureByWER(r, h, threshold, chunkSize):
     indexReference = 0
-    indexHypothesis =0
+    indexHypothesis = 0
     textList = []
     isFinal = False
     substitution = 0
     deletion = 0
     insertion = 0
+    correct = 0
+
     # print("\nlength r :",id(r),"-",len(r)-1,"\nlength h :",id(h),"-",len(h)-1)
 
     while(not isFinal):
         print("-------------------------\n")
-        lastIndex = getChunk(r[indexReference:indexReference+chunkSize],h[indexHypothesis:chunkSize+indexHypothesis],threshold )
+        lastIndex = getChunk(r[indexReference:indexReference+chunkSize],
+                             h[indexHypothesis:chunkSize+indexHypothesis], threshold)
         # for string format [:num+1] because getChunk return index(index start with 0)
 
-        print("last index :",lastIndex)
-        # not match
-        if(lastIndex==[]):
-            data = get_word_error_rate(r[indexReference:],h[indexHypothesis:])
+        print("last index :", lastIndex)
+        # not match or lower threshold
+        if(lastIndex == []):
+            data = WER(r[indexReference:], h[indexHypothesis:])
             indexReference += len(r[indexReference:])-1
             indexHypothesis += len(h[indexHypothesis:])-1
-            textList.append((r[indexReference:],h[indexHypothesis:]))
+            textList.append((r[indexReference:], h[indexHypothesis:]))
             isFinal = True
             # return 0
-
-        elif(lastIndex[0]==0 and lastIndex[1]==0):
-
+        # chunk is last sentence
+        elif(lastIndex[0] == 0 and lastIndex[1] == 0):
             if((indexReference+chunkSize >= len(r)-1) or (indexHypothesis+chunkSize >= len(h)-1)):
-                data = get_word_error_rate(r[indexReference:],h[indexHypothesis:])
+                data = WER(r[indexReference:], h[indexHypothesis:])
                 indexReference += len(r[indexReference:])-1
                 indexHypothesis += len(h[indexHypothesis:])-1
-                textList.append((r[indexReference:],h[indexHypothesis:]))
+                textList.append((r[indexReference:], h[indexHypothesis:]))
                 isFinal = True
+        # threshold ok
         else:
-            data = get_word_error_rate(r[indexReference:indexReference+lastIndex[0]+1],h[indexHypothesis:indexHypothesis+lastIndex[1]+1])                
-            textList.append((r[indexReference:indexReference+lastIndex[0]+1],h[indexHypothesis:indexHypothesis+lastIndex[1]+1]))
+            data = WER(r[indexReference:indexReference+lastIndex[0]+1],
+                       h[indexHypothesis:indexHypothesis+lastIndex[1]+1])
+            textList.append((r[indexReference:indexReference+lastIndex[0]+1],
+                             h[indexHypothesis:indexHypothesis+lastIndex[1]+1]))
             # sum 1 because next char
             indexReference += lastIndex[0]+1
             indexHypothesis += lastIndex[1]+1
         substitution += data[0]
         deletion += data[1]
         insertion += data[2]
-        print(">>> index :",indexReference," , ",indexHypothesis)
+        correct += data[3]
+        print(">>> index :", indexReference, " , ", indexHypothesis)
 
         if((indexReference > (len(r)-1) and indexHypothesis > (len(h)-1)) or isFinal):
             # return textList,indexReference,indexHypothesis
             break
-
-    print("\n\n>>>",substitution,deletion,insertion)
+    print(textList)
+    print("\n\n>>>", substitution, deletion, insertion, correct)
     result = float((substitution+deletion+insertion)/len(r))
-    return result
+    resultDict = {"WER": result*100, "substitution": substitution,
+                  "deletion": deletion, "insertion": insertion, "correct": correct}
+    return resultDict
 
 
 if __name__ == "__main__":
+    import os
+    import psutil
+    import time
+    import json
+    # print("PID : ",os.getpid())
+    # time.sleep(2)
 
     r = "เราไปทำงานที่นี่น้า"
-    h = "เรไปทำงานที่นี่น้ะ"
-    threshold =4
-    chunkSize = 10 
+    h = "เรไปทงานที่นี่น้ะ"
 
-    print(measureByWER(r, h,threshold,chunkSize))
-    print("length r :",len(r)-1,"\nlength h :",len(h)-1)
+    threshold = 10
+    chunkSize = 1000
+    rPath = "C:/Users\Admin\Desktop\เทียบเฉลย\correct/test.txt"
+    hPath = "C:/Users\Admin\Desktop\เทียบเฉลย/raw/test.txt"
+
+    with codecs.open(rPath,'r',encoding="utf-8") as file:
+        r = file.read().replace(" ","")
+    with codecs.open(hPath,'r',encoding="utf-8") as file:
+        h = file.read().replace(" ","")
+
+    startTime = time.time()
+    data = measureByWER(r, h, threshold, chunkSize)
+    endTime = time.time()
+
+    print("WER : ", data['WER'])
+    # print("length r :",len(r)-1,"\nlength h :",len(h)-1)
+
+    process = psutil.Process(os.getpid())
+    memeryUse = process.memory_info().rss
+    provText = {
+        "Start": startTime,
+        "endTime": endTime,
+        "duration": endTime-startTime,
+        "substitution": data["substitution"],
+        "deletion": data["deletion"],
+        "insertion": data["insertion"],
+        "correct": data["correct"],
+        "WER": data["WER"],
+        "threshold": threshold,
+        "chunkSize": chunkSize,
+        "PID": os.getpid(),
+        "memeryUse": memeryUse
+    }
+    with codecs.open("./output/output_prove.json", 'w', encoding="utf-8") as file:
+        file.write(str(json.dumps(provText, indent=4)))
