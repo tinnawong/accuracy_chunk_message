6af8f4ef7c6cb3c03dd4f24409d299422b564559
tinnawong <tinnawong2010@hotmail.com>
2020-10-19 17:16:39 +0700

    add funcion write html from golang

--- prove2.py
+++ prove2.py
@@ -10,10 +10,43 @@ if __name__ == "__main__":
     import psutil
     import codecs
     import requests
+    import requests_cache
     # 0 substitution
     # 1 deletion
     # 2 insertion
     # 3 correct
+
+
+    def writeHtmlFromGolang(fileName,response):
+        html ="""
+            <!DOCTYPE html>
+            <html>
+                <head>
+                <meta charset='utf-8'>
+                <title></title>
+                <meta name='viewport' content='width=device-width, initial-scale=1'>
+                <style>
+                .y {
+                    background-color: #ffa50099;
+                }
+
+                .r {
+                    background-color: #ff00004d;
+                }
+
+                .g {
+                    background-color: #0080004d;
+                }
+                </style>
+            </head>
+            <body></html>
+        """
+        for item in response:
+            html += str(item)+":"+str(response[item])+"<br>"
+
+        html+="/body"
+        with codecs.open("./output/5th golang/{}.html".format(fileName), 'w', encoding="utf-8") as file:
+            file.write(html)
 
     def genPathFile(directory,keyFile=None):
         files = os.listdir(directory)
@@ -26,19 +59,19 @@ if __name__ == "__main__":
                     allPath.append(os.path.join(directory,fileName))
         return allPath
 
-    rDir = "C:/Users\Admin\Desktop\เทียบเฉลย\สำหรับทดสอบ/correct test/"
-    hDir = "C:/Users\Admin\Desktop\เทียบเฉลย\สำหรับทดสอบ/raw test/"
+    rDir = "C:/Users\Admin\Desktop\เทียบเฉลย\ไฟล์ทดสอบเพิ่มเติม/สำหรับ golang/corect/"
+    hDir = "C:/Users\Admin\Desktop\เทียบเฉลย\ไฟล์ทดสอบเพิ่มเติม/สำหรับ golang/raw/"
 
-    rPath = genPathFile(rDir,"" )[:3]
-    hPath = genPathFile(hDir,"")[:3]
+    rPath = genPathFile(rDir,"3393" )
+    hPath = genPathFile(hDir,"3393")
 
     threshold = 100
     # size = range(2000,3000)
-    size= [3000]
+    size= [4000]
     print(">>> size :",size)
     
     # service test
-    url = "http://127.0.0.1:5050/accuracy"
+    url = "http://127.0.0.1:5051/acc"
 
     for j,path in enumerate(rPath):
         for i,chunkSize in enumerate(size):
@@ -54,41 +87,46 @@ if __name__ == "__main__":
                 fileh = fileh.replace("\r","")
 
                 fileName = os.path.splitext(os.path.split(hPath[j])[1])[0]
-            resultProve = testRun(filer,fileh,rPath[j],hPath[j],chunkSize,threshold,fileName,createHtml=False)
+            # resultProve = testRun(filer,fileh,rPath[j],hPath[j],chunkSize,threshold,fileName,createHtml=False)
             
             # send path file
             text = dict({"pathAnswer": rPath[j],"pathSubject":hPath[j]})
             # send text
-            text = dict({"textAnswer": filer,"textSubject":fileh})
+            # text = dict({"textAnswer": filer,"textSubject":fileh})
             try:
                 print(">>> connect glex service")
                 response = requests.get(url, params=text)
-
+                
                 # print(">>> respones : ",json.loads(response.text))
                 if(response.status_code == 200):
                     try:
                         print(">>> response glex service status ok")
                         response = json.loads(response.text)["result"]
-                        math = True
-                        checkList =["insertion","deletion","substitution","correction","referenceLength","hypothesisLength"]
-                        for check in checkList:
-                            if(resultProve[check] == response[check]):
-                                print(">>> {} match : {} = {}".format(check,resultProve[check] , response[check]))
-                            else:
-                                print(">>> {} not match : {} != {}".format(check,resultProve[check] , response[check]))
-                                math = False
-                        if(not math):
-                            writeHtml(resultProve,fileName)
-                        else:
-                            print(">>> math all chekc list")
+                        writeHtmlFromGolang(fileName,response)
+
+                        # math = True
+                        # checkList =["insertion","deletion","substitution","correction","referenceLength","hypothesisLength"]
+                        # for check in checkList:
+                        #     if(resultProve[check] == response[check]):
+                        #         print(">>> {} match : {} = {}".format(check,resultProve[check] , response[check]))
+                        #     else:
+                        #         print(">>> {} not match : {} != {}".format(check,resultProve[check] , response[check]))
+                        #         math = False
+                        # if(1):
+                        #     writeHtml(resultProve,fileName)
+                        # else:
+                        #     print(">>> math all chekc list")
+                        
                     except Exception as e:
                         print(e)
                         # return {"status": "failed","message":"can not format json from glex service "}
             except Exception as e:
                         print(e)
                         # return {"status": "failed","message":"can not format json from glex service "}
-        with codecs.open("./output/{}[{}].json".format(fileName,j), 'w', encoding="utf-8") as file:
-            file.write(json.dumps(resultProve,indent=4,ensure_ascii=False))
+
+
+        # with codecs.open("./output/{}[{}].json".format(fileName,j), 'w', encoding="utf-8") as file:
+        #     file.write(json.dumps(resultProve,indent=4,ensure_ascii=False))
 
 
     def finished():
