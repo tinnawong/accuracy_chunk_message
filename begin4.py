1d34a83579224da1f2c9fc3d3c55a1cb4d421a74
tinnawong <tinnawong2010@hotmail.com>
2020-10-20 17:12:07 +0700

    update function genPathFile

--- begin4.py
+++ begin4.py
@@ -6,24 +6,28 @@ def genPathFile(directory, keyFile=None):
         files = os.listdir(directory)
         allPath = []
         for fileName in files:
+            print(fileName)
             if(not keyFile):
-                allPath.append(os.path.join(directory, fileName))
+                if os.path.isfile(os.path.join(directory, fileName)):
+                    allPath.append(os.path.join(directory, fileName))
             else:
                 if(keyFile in fileName):
-                    allPath.append(os.path.join(directory, fileName))
+                    if os.path.isfile(os.path.join(directory, fileName)):
+                        allPath.append(os.path.join(directory, fileName))
         return allPath
 
-pathFile ="C:/Users\Admin\Desktop\เทียบเฉลย\ไฟล์ทดสอบเพิ่มเติม/สำหรับ golang/corect/"
+pathFile ="C:/Users\Admin\Desktop\เทียบเฉลย\ไฟล์ทดสอบเพิ่มเติม"
 
-for i,path in enumerate(genPathFile(pathFile,"")):
-    print("take file [{}]".format(i))
-    with codecs.open(path,"r",encoding="utf-8-sig") as ff:
-        gg = ff.read()
-        # f = ff.read().lower()
-        # f = f.replace(" ", "")
-        # f = f.replace("\n", "")
-        # gg = f.replace("\r", "")
+print(genPathFile(pathFile,""))
+# for i,path in enumerate(genPathFile(pathFile,"")):
+#     print("take file [{}]".format(i))
+#     with codecs.open(path,"r",encoding="utf-8-sig") as ff:
+#         # gg = ff.read()
+#         f = ff.read().lower()
+#         f = f.replace(" ", "")
+#         f = f.replace("\n", "")
+#         gg = f.replace("\r", "")
 
         
-    with codecs.open(path,"w",encoding="utf-8") as f:
-        f.write(gg)
+#     with codecs.open(path,"w",encoding="utf-8") as f:
+#         f.write(gg)
