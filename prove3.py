50e1eb360e1941506416e7f6f3538a16fc0b77fe
DESKTOP-N2I08PG\Broadcaster2 <tinnawong2010@hotmail.com>
2020-11-26 16:27:38 +0700

    update for run go

--- prove3.py
+++ prove3.py
@@ -2,22 +2,31 @@ from prove import logProcess
 from multiprocessing import Process
 import time
 import os
-def runGo():
+def runGo(pathRef,pathHyp,output):
     # file go
-    os.system("cd /d E:\golang/accuracy\cmd\example/room && go run .")
+    tt = """ -ldflags="-X 'gitlab.spinsoft.co.th/transcription/accuracy/comparing/config.Version=v100' -X 'main.PATHREF={}' -X 'main.PATHHYP={}' -X gitlab.spinsoft.co.th/transcription/accuracy/comparing/config.Output={}' " """.format(pathRef,pathHyp,output)
+    os.system("cd /d E:\golang/accuracy\cmd\example/room && go run {} .".format(tt))
 
 
 if __name__ == "__main__":
     # for monitor cpu and ram of accuracy golang process
-    fileName = "60kb 3913"
-    print(os.getpid())
-    procs = []
-    p2 = Process(target=logProcess, args=(fileName, "output/7th monitor system\golang prove"))
-    p2.start()
-    procs.append(p2)
-    time.sleep(2)
-    p1 = Process(target=runGo, args=())
-    p1.start()
-    procs.append(p1)
-    for p in procs:
-        p.join()
+    from prove import genPathFile
+    output = "E:/python/prove-WER-method/output/"
+    PATHREF = "T:/Shared drives/งานบริษัท/เทียบเฉลย accuracy/ไฟล์ทดสอบเพิ่มเติม/correct/"
+    PATHHYP = "T:/Shared drives/งานบริษัท/เทียบเฉลย accuracy/ไฟล์ทดสอบเพิ่มเติม/raw/"
+    getRef = genPathFile(PATHREF)
+    getHyp = genPathFile(PATHHYP)
+    
+    for i,getPath in enumerate(getRef):
+        fileName = os.path.splitext(os.path.split(getHyp[i])[1])[0]
+        print(os.getpid())
+        procs = []
+        p2 = Process(target=logProcess, args=(fileName, output))
+        p2.start()
+        procs.append(p2)
+        time.sleep(2)
+        p1 = Process(target=runGo, args=(getPath,getHyp[i],output))
+        p1.start()
+        procs.append(p1)
+        for p in procs:
+            p.join()
