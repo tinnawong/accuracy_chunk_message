fb8b56ce55fad90e73636698319c0464ab3ab86e
tinnawong <tinnawong2010@hotmail.com>
2021-01-04 11:30:08 +0700

    refactor code

--- makeTool/renameFile.py
+++
@@ -1,7 +0,0 @@
-
-if __name__ == "__main__":
-    import os
-    from coppyMaxRam import genPathFile
-    path ="E:/python/prove-WER-method/output/11th monitor for setting (ram)/"
-    for pathFile in genPathFile(path):
-        os.rename(pathFile,str(pathFile).replace("'",""))
